79c1532b3059c611d323b0b073a6a625ba73a11f
cuizhiquan <cuizq@dachentech.com.cn>
2019-11-04 10:57:26 +0800

    DataSourceConfig

---
+++ src/main/java/com/seata/storage/DataSourceConfiguration.java
@@ -0,0 +1,44 @@
+package com.seata.storage;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * @author cuizhiquan
+ * @Description
+ * @date 2019/11/1 20:55
+ * @Copyright (c) 2017, DaChen All Rights Reserved.
+ */
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
+    }
+
+    @Primary
+    @Bean("dataSource")
+    public DataSourceProxy dataSource(DataSource druidDataSource){
+        return new DataSourceProxy(druidDataSource);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSourceProxy dataSourceProxy)throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSourceProxy);
+        sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
+        return sqlSessionFactoryBean.getObject();
+    }
+}
